99fe67734ec612846818a49f209b43203bed7045
Fauzan Fathurrahman <fauzan@radyalabs.com>
2020-02-23 19:16:54 +0700

    Implement Soft Delete

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,19 +14,24 @@
                 <thead>
                     <th>Image</th>
                     <th>Title</th>
+                    <th></th>
                 </thead>
                 <tbody>
                     @foreach ($posts as $post)
                         <tr>
                             <td>
-                            <img src="{{ asset('storage/' . $post->image) }}" width="150px" height="150px" alt="{{$post->title}}">
+                            <img src="{{ asset('storage/' . $post->image) }}" width="130px" height="150px" alt="{{$post->title}}">
                             </td>
                             <td>
                                 {{ $post->title }}
                             </td>
                             <td>
                                 <button class="btn btn-info btn-sm">Edit</button>
-                                <button class="btn btn-danger btn-sm">Trash</button>
+                                <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm">Trash</button>
+                                </form>
                             </td>
                         </tr>
                     @endforeach
