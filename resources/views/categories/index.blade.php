d20fb9d309a96375da56a689a10bf7f110d3a93a
Fauzan Fathurrahman <fauzan@radyalabs.com>
2020-03-02 20:32:41 +0700

    Add Post Category Relationship

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -13,6 +13,7 @@
             <table class="table">
                 <thead>
                     <th>Name</th>
+                    <th>Posts Count</th>
                     <th></th>
                 </thead>
                 <tbody>
@@ -20,6 +21,9 @@
                         <tr>
                             <td>
                                 {{ $category->name }}
+                            </td>
+                            <td>
+                                {{ $category->posts->count() }}
                             </td>
                             <td>
                             <a href="{{ route('categories.edit',$category->id) }}" class="btn btn-info btn-sm">
