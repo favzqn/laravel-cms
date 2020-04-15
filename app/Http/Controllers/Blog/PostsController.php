6e8e60b94e016f009ef69173678867550b548e5c
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2020-04-15 13:46:50 +0700

    Tags and Categories

--- app/Http/Controllers/Blog/PostsController.php
+++ app/Http/Controllers/Blog/PostsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Blog;
 
 use App\Post;
+use App\Category;
+use App\Tag;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -10,5 +12,40 @@ class PostsController extends Controller
 {
     public function show(Post $post){
         return view('blog.show')->with('post', $post);
+    }
+
+    public function category(Category $category){
+
+        $search = request()->query('search');
+
+        if ($search) {
+            $posts = $category->posts()->where('title','LIKE',"%{$search}%")->simplePaginate(4);
+        }else{
+            $posts = $category->posts()->simplePaginate(4);
+        }
+
+        return view('blog.category')
+        ->with('category', $category)
+        ->with('posts', $posts)
+        ->with('categories', Category::all())
+        ->with('tags', Tag::all());
+    }
+
+    public function tag(Tag $tag){
+
+        $search = request()->query('search');
+
+        if ($search) {
+            $posts = $tag->posts()->where('title','LIKE',"%{$search}%")->simplePaginate(4);
+        }else{
+            $posts = $tag->posts()->simplePaginate(4);
+        }
+
+        return view('blog.tag')
+        ->with('tag', $tag)
+        ->with('categories', Category::all())
+        ->with('tags', Tag::all())
+        ->with('posts', $tag->posts()
+        ->simplePaginate(4));
     }
 }
